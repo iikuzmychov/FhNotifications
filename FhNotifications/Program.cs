7dedcea1ae6de1923ba50e89a90f9ea19124a055
kuzcode <igormakw228@gmail.com>
2021-03-20 17:24:51 +0200

    Добавленна проверка на количество сущностей программы.

--- FhNotifications/Program.cs
+++ FhNotifications/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +15,19 @@ namespace FhNotifications
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            using (var mutex = new Mutex(true, Application.ProductName, out bool createdNew))
+            {
+                if (createdNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainForm());
+                }
+                else
+                {
+                    MessageBox.Show("Приложение уже было запущенно, возможно оно свёрнуто в трей", "Попытка повторного запуска", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
         }
     }
 }
